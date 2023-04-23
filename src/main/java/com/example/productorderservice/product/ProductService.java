b4117f81c29afebfe958f97597186387c8dfca7f
Taemin Im <devTaemin@gmail.com>
2023-04-23 15:24:01 +0900

    adopt jpa

--- src/main/java/com/example/productorderservice/product/ProductService.java
+++ src/main/java/com/example/productorderservice/product/ProductService.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.transaction.Transactional;
+
 @RestController
 @RequestMapping("/products")
 class ProductService {
@@ -17,6 +19,7 @@ class ProductService {
     }
 
     @PostMapping
+    @Transactional
     public ResponseEntity<Void> addProduct(@RequestBody final AddProductRequest request) {
         final Product product = new Product(request.name(), request.price(), request.discountPolicy());
 
